2c0f767d1a3a18175a1f958b5b5da66c72de187c
Berkayszk <testmail>
2023-10-15 19:39:37 +0300

    Initial Commit

---
+++ ViewControllerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewControllerProject
+//
+//  Created by Berkay Sazak on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
